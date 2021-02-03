2b61ad9b85b9d64ce8f12bcad1c791b1007dd596
reejit <70316178+reejit@users.noreply.github.com>
2021-02-03 20:53:16 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -10,66 +10,66 @@ except ImportError:
     )
 
     parser.add_argument(
-        "--token",
+        "TOKEN",
         help="Bot token",
         required=True,
     )
     parser.add_argument(
-        "--api-id",
+        "API_ID",
         help="Telegram api_id, get it from my.telegram.org/apps",
         type=int,
         required=True,
     )
     parser.add_argument(
-        "--api-hash",
+        "API_HASH",
         help="Telegram api_hash, get it from my.telegram.org/apps",
         required=True,
     )
     parser.add_argument(
-        "--mongodb-uri",
+        "MONGO_FB_URI",
         help="MongoDB URI, if you don't provide the bot will lack some cool features",
         required=True,
     )
     parser.add_argument(
-        "--sudo-users",
+        "SUDO_USERS",
         help="List of user ids, separate by underscore (example: 1_3)",
         type=str,
         required=True,
     )
     parser.add_argument(
-        "--group",
+        "GROUP",
         help="Id of the group where the bot streams",
         type=int,
         required=True,
     )
     parser.add_argument(
-        "--users-must-join",
+        "USERS_MUST_JOIN",
         help="If provided, only members of the group can use the bot",
         type=bool,
         required=False,
         default=False,
     )
     parser.add_argument(
-        "--lang",
+        "LANG",
         help="Bot language, choose from strings/",
         required=True,
     )
     parser.add_argument(
-        "--dur-limit",
+        "DUR_LIMIT",
         help="Video download duration limit (in minutes)",
         type=int,
         required=True,
     )
     args = parser.parse_args()
-    API_ID = args.api_id
-    API_HASH = args.api_hash
-    TOKEN = args.token
-    MONGO_DB_URI = args.mongodb_uri
+    API_ID = args.API_ID
+    API_HASH = args.API_HASH
+    TOKEN = args.TOKEN
+    MONGO_DB_URI = args.MONGO_DB_URI
     SUDO_USERS = list(map(int, args.sudo_users.split("_")))
-    GROUP = args.group
-    USERS_MUST_JOIN = args.users_must_join
-    LANG = args.lang
-    DUR_LIMIT = args.dur_limit
+    GROUP = args.GROUP
+    USERS_MUST_JOIN = args.USERS_MUST_JOIN
+    LANG = args.LANG
+    DUR_LIMIT = args.DUR_LIMIT
     LOG_GROUP = GROUP if MONGO_DB_URI not in ("", None) else None
     SUDO_FILTER = filters.user(SUDO_USERS)
 
